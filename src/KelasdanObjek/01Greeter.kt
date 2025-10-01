725171dbdd1bf5613ba5b8e17e57b1f614ae9da1
roisulfikri <roisulfikri8@gmail.com>
2025-10-01 20:29:04 +0700

    Percobaan Greeter

---
+++ src/KelasdanObjek/01Greeter.kt
@@ -0,0 +1,14 @@
+package KelasdanObjek
+
+class Greeter {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
+    }
+}
