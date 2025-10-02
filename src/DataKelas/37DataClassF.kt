ca9caea49774bef16a3a395ffa111947eb9d9a26
roisulfikri <roisulfikri8@gmail.com>
2025-10-02 18:57:03 +0700

    Percobaan Data Class F

---
+++ src/DataKelas/37DataClassF.kt
@@ -0,0 +1,10 @@
+package DataKelas
+
+fun main() {
+    val meri = Student("Meri", 20)
+    // Destructuring Declaration with componentN() method
+    val name = meri.component1()
+    val age = meri.component2()
+    println("Name = $name")
+    println("Age = $age")
+}
